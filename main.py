58c8532b7d4b3462e5e84f01c472a1f746b2cfa8
Antonio Manzari <antoniomanzari@gmail.com>
2022-08-13 11:39:37 -0400

    writing output to file success

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 groupUrl = 'https://steamcommunity.com/groups/LanguageLearningGames'
-
 groupPageResponse = requests.get(groupUrl)
-
 groupPageHtml = BeautifulSoup(groupPageResponse.text, features='html.parser')
-
 results = groupPageHtml.find_all('div', {'class': 'group_associated_game'})
 
-for r in results:
+textFile = open(f'games.txt', 'w')
+print("Writing file... ")
+
+for index, r in enumerate(results):
     gameTitle = r.text
+    print(f"adding {index + 1}. {gameTitle.strip()}... ")
     communityUrl = r.find("a", recursive=False)['href']
     singleGameResponse = requests.get(communityUrl)
     gamePageHtml = BeautifulSoup(singleGameResponse.text, features='html.parser')
     result = gamePageHtml.find('a', {'id': 'app_header_view_store_page_btn'})
     storeUrl = result['href']
-    print(gameTitle + ': ' + storeUrl)
+    textFile.write(f'{index + 1}, {gameTitle.strip()}, {storeUrl}' + '\n') 
 
-# The link returned is a community page link.
-# Next, scrape that URL to get the store page link
+textFile.close()
+print("finished! file saved")
